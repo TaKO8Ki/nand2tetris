7619258764da5d55d8171db9635ce19fa10778f1
TaKO8Ki <takoyaki0316@gmail.com>
2020-04-19 12:38:16 +0900

    add fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,68 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// define screen size
+    @8192
+    D=A
+    @number_of_pixel
+    M=D
+
+    @R0
+    M=0     // Screen is empty at the start
+(LOOP)
+    @KBD
+    D=M
+    @KEY_PUSHED
+    D;JNE   // if KBD != 0 then goto KEY_PUSHED
+(KEY_NOT_PUSHED)
+    @R1
+    M=0     // R0=0
+    @CHECK_STATE_CHANGE
+    0;JMP
+(KEY_PUSHED)
+    @R1
+    M=1     // R1=1
+(CHECK_STATE_CHANGE)    // if R0 != R1 then screen is update
+    @R0
+    D=M     // D=R0
+    @R1
+    D=D-M   // D=R0-R1
+    @LOOP
+    D;JEQ   // if D == 0 then goto LOOP
+    @i
+    M=0
+    @R1
+    D=M
+    @R0
+    M=D
+    @EMPTY_SCREEN_LOOP
+    D;JEQ
+(FILL_SCREEN_LOOP)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @LOOP
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    MD=M+1
+    @FILL_SCREEN_LOOP
+    0;JMP
+(EMPTY_SCREEN_LOOP)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @LOOP
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    MD=M+1
+    @EMPTY_SCREEN_LOOP
+    0;JMP
+
